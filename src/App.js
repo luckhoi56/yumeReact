60e78c4fbe7c01e81a806525b8690420ecef01de
Eric Luc <luckhoi89@gmail.com>
2020-09-05 01:10:03 -0700

    layout added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Grid from './components/layout/Grid'
 function App(){
   return(
-    <Grid>
-  <Grid.Row columns={3}>
-    <Grid.Column>1</Grid.Column>
-    <Grid.Column>2</Grid.Column>
-    <Grid.Column>3</Grid.Column>
+  <Grid>
+  <Grid.Row columns={5}>
+    <Grid.Column></Grid.Column>
+    <Grid.Column></Grid.Column>
+    <Grid.Column>Yume House Pho</Grid.Column>
+    <Grid.Column></Grid.Column>
+    <Grid.Column>Order Now</Grid.Column>
   </Grid.Row>
-</Grid>
+  </Grid>
+  
   )
 }
 export default App;
